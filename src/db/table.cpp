2ea462167936dffdf0ff0d65d42f6b9c4b8e2b2e
svenFeng <1280142276@qq.com>
2017-02-25 13:25:58 +0800

    [remove] record read/write in table

--- src/db/table.cpp
+++ src/db/table.cpp
@@ -6,7 +6,7 @@
 #include <boost/filesystem.hpp>
 #include <utility>
 
-#include "../index/bpTree.h"
+#include "bpTree.h"
 #include "table.h"
 #include "util.h"
 #include "io.h"
@@ -30,45 +30,35 @@ std::string Table::get_file_abs_path(bool is_index)const {
     return file_path;
 }
 
-RecordList Table::read_record(const std::vector<Pos> &pos_lst)const{
-    std::map<size_t, std::vector<Pos>> block_offsets_map;
-    for (auto &&item : pos_lst) {
-        size_t block_num = item / BLOCK_SIZE;
-        Pos offset = item % BLOCK_SIZE;
-        block_offsets_map[block_num].push_back(offset);
-    }
-    IO io(get_file_abs_path(false));
-    size_t record_size = property.get_record_size();
-    RecordList record_lst(property);
-    for (auto &&item: block_offsets_map) {
-        DB::Type::Bytes data_block = io.read_block(item.first);
-        for (auto &&offset : item.second) {
-            auto beg = data_block.data()+offset;
-            DB::Type::Bytes record_tuple(beg, beg+record_size);
-            record_lst.record_tuple_lst.push_back(record_tuple);
+DB::Type::Bytes Table::tuple_get_col_value(const DB::Type::Bytes &tuple,
+                                const std::string &col_name) const {
+    Pos pos = 0;
+    for (auto &&item : property.col_property) {
+        if (item.first == col_name) {
+            auto col_type = item.second.first;
+            auto col_size = item.second.second;
+            if (col_type == DB::Enum::VARCHAR) {
+                std::string str(col_size, '\0');
+            }
         }
+        pos += item.second.second;
     }
-    return record_lst;
 }
 
-void Table::write_record(const DB::Type::BytesList &tuple_lst,
-                         const std::vector<Pos> &pos_lst) {
-    if (tuple_lst.size() != pos_lst.size()) {
-        throw std::runtime_error("Error: Table::write_record: "
-                                         "record_lst and pos_lst length not equal.");
-    }
-    std::map<size_t, std::vector<std::pair<Pos, DB::Type::Bytes>>> block_offsets_map;
-    for (size_t i = 0; i < pos_lst.size(); ++i) {
-        size_t block_num = pos_lst[i] / BLOCK_SIZE;
-        Pos offset =  pos_lst[i] % BLOCK_SIZE;
-        block_offsets_map[block_num].push_back(std::make_pair(offset, tuple_lst[i]));
-    }
-    IO io(get_file_abs_path(false));
-    for (auto &&item : block_offsets_map) {
-        DB::Type::Bytes block_buffer = io.read_block(item.first);
-        for (auto &&second : item.second) {
-            std::copy(second.second.begin(), second.second.end(), block_buffer.data()+second.first);
-        }
-        io.write_block(block_buffer, item.first);
-    }
+// SQL
+DB::Type::Bytes Table::make_tuple(std::initializer_list<std::string> args) {
+}
+
+void Table::insert(std::initializer_list<std::string> args) {
+}
+
+void Table::update(const std::string &col_name,
+                   const std::string &op,
+                   const std::string &value) {
+}
+
+void Table::remove(const std::string &col_name,
+                   const std::string &op,
+                   const std::string &value) {
+
 }
--- src/db/table.h
+++ src/db/table.h
@@ -3,10 +3,10 @@
 
 #include <string>
 #include <map>
+#include <initializer_list>
 
 #include "record.h"
 #include "util.h"
-#include "table_property.h"
 
 class Table {
 public:
@@ -15,20 +15,48 @@ public:
 //            :table_name(table.table_name), BLOCK_SIZE(table.BLOCK_SIZE){}
 //    Table(Table &&table)
 //            :table_name(table.table_name), BLOCK_SIZE(table.BLOCK_SIZE){}
-    Table(const TableProperty &property):property(property){}
+    Table(const DB::Type::TableProperty &property):property(property){}
 
     // get
     std::string get_file_abs_path(bool is_index)const;
+    // tuple operate
+    DB::Type::Bytes tuple_get_col_value(const DB::Type::Bytes &tuple,
+                                        const std::string &col_name)const;
 
-    //read
-    RecordList read_record(const std::vector<DB::Type::Pos> &pos_lst)const;
-
-    // write
-    void write_record(const DB::Type::BytesList &tuple_lst,
-                      const std::vector<DB::Type::Pos> &pos_lst);
+    // sql
+    DB::Type::Bytes make_tuple(std::initializer_list<std::string> args);
+//    template <typename T, typename ...Args>
+//    static DB::Type::Bytes make_tuple(const T &t, const Args& ...args);
+    void insert(std::initializer_list<std::string> args);
+    void update(const std::string &col_name,
+                const std::string &op,
+                const std::string &value);
+    void remove(const std::string &col_name,
+                const std::string &op,
+                const std::string &value);
 
 private:
-    TableProperty property;
+    DB::Type::TableProperty property;
+
+    // function
+//    template <typename T>
+//    static DB::Type::Bytes make_tuple(const T &t);
 };
+
+//template <typename T>
+//DB::Type::Bytes Table::make_tuple(const T &t){
+//    DB::Type::Bytes tuple(sizeof(t));
+//    std::memcpy(tuple.data(), &t, sizeof(t));
+//    return tuple;
+//}
+//
+//template <typename T, typename ...Args>
+//DB::Type::Bytes Table::make_tuple(const T &t, const Args& ...args){
+//    DB::Type::Bytes tuple(sizeof(t));
+//    std::memcpy(tuple.data(), &t, sizeof(t));
+//    DB::Type::Bytes bytes = make_tuple(args...);
+//    tuple.insert(tuple.end(), bytes.begin(), bytes.end());
+//    return tuple;
+//}
 
 #endif

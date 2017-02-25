7d4f63999e826e3e04f1382f3183aee01a780455
svenFeng <1280142276@qq.com>
2017-02-25 13:26:59 +0800

    [add] en_bytes/de_bytes

--- src/db/util.h
+++ src/db/util.h
@@ -7,6 +7,9 @@
 
 #include <stdexcept>
 #include <vector>
+#include <cstring>
+#include <string>
+#include <map>
 
 namespace DB {
     namespace Enum {
@@ -16,32 +19,63 @@ namespace DB {
             FLOAT,
             VARCHAR
         };
+
+        enum : char {
+            RECORD_SUFFIX = 1,
+            INDEX_SUFFIX,
+            POS_SUFFIX
+        };
     }
 
     namespace Const {
-        const size_t BLOCK_SIZE = 4096;
+        const size_t BLOCK_SIZE = 100;
+        const std::string DB_FILE_DIR_PATH = DB::Function::get_db_file_dir_path();
     }
 
     namespace Function {
-//        size_t get_col_type_size(char col_type){
-//            switch (col_type) {
-//                case DB::Enum::CHAR :
-//                    return 1;
-//                case DB::Enum::INT :
-//                    return 4;
-//                case DB::Enum::FLOAT :
-//                    return 8;
-//                default:
-//                    throw std::runtime_error("Error: col_type");
-//            }
-//        }
+        template <typename T>
+        void de_bytes(const std::vector<char> &bytes, T &t){
+            std::memcpy(&t, bytes.data(), sizeof(t));
+        }
+        template <>
+        inline void de_bytes<std::string>(const std::vector<char> &bytes, std::string &t){
+            t = std::string(bytes.begin(), bytes.end());
+        }
+
+        template <typename T>
+        void en_bytes(std::vector<char> &bytes, const T &t){
+            bytes = std::vector<char>(sizeof(t));
+            std::memcpy(bytes.data(), &t, sizeof(t));
+        }
+        template <>
+        inline void en_bytes<std::string>(std::vector<char> &bytes, const std::string &t){
+            bytes = std::vector<char>(t.begin(), t.end());
+        }
+
+        std::string get_db_file_dir_path();
     }
 
     namespace Type {
         using Pos = size_t;
-        using Byte = char ;
-        using Bytes = std::vector<Byte> ;
+        using PosList = std::vector<Pos>;
+        using Byte = char;
+        using Bytes = std::vector<Byte>;
         using BytesList = std::vector<Bytes>;
+
+        struct TableProperty {
+            std::string table_name;
+            std::string key;
+            std::map<std::string, std::pair<char, size_t>> col_property;
+
+            TableProperty()= delete;
+            TableProperty(const std::string &table_name,
+                          const std::string &key,
+                          const std::map<std::string, std::pair<char, size_t >> &col_property)
+                    :table_name(table_name), key(key),col_property(col_property){}
+
+            std::string get_file_abs_path(char file_suffix)const;
+            size_t get_record_size()const;
+        };
     }
 
 }
